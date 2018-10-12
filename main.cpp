686dc7c2463046c69125590f197037062aacd8ee
Day-Sydney <days@bournemouth.ac.uk>
2018-10-12 10:32:43 +0100

    working image bmp Magick lib

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <cstdlib>
 #include <Magick++.h>
+#include <memory>
 
 int main()
 {
     const size_t width = 100;
     const size_t height = 100;
     const size_t pixelDepth = 3;
-    unsigned char image[width*height*pixelDepth];
+    std::unique_ptr<unsigned char []> image=std::make_unique<unsigned char[]>(width*height*pixelDepth);
 
     for (size_t i = 0; i<width*height*pixelDepth; ++i)
     {
