af0e0b57085e564b3dfdceb2789ec64837f85b52
Day-Sydney <days@bournemouth.ac.uk>
2018-10-12 10:28:51 +0100

    working image bmp Magick lib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
-#include <stdlib>
+#include <cstdlib>
+#include <Magick++.h>
 
-int main(){
+int main()
+{
+    const size_t width = 100;
+    const size_t height = 100;
+    const size_t pixelDepth = 3;
+    unsigned char image[width*height*pixelDepth];
+
+    for (size_t i = 0; i<width*height*pixelDepth; ++i)
+    {
+        image[i] = 128;
+    }
+    Magick::Image output(width,height,"RGB",Magick::CharPixel,image);
+    output.depth(16);
+    output.write("test.bmp");
     return EXIT_SUCCESS;
 }
